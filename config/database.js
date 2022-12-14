e93e9aa5479e34d8e9e332a49650a48b72fa70a3
Sebastian <eabastian@gmail.com>
2022-12-14 14:45:08 -0600

    changed db

--- config/database.js
+++ config/database.js
@@ -2,12 +2,19 @@ const mysql = require('mysql')
 const util = require('util')
 
 const pool = mysql.createPool({
-    connectionLimit:10,
-    host:'node-server.mysql.database.azure.com',
+    connectionLimit : 1000,
+    host:'nodedb.mysql.database.azure.com',
     user:'myadmin',
     password:'TallerNode1',
     database:'empleados'
 });
+// const pool = mysql.createPool({
+//     connectionLimit : 5,
+//     host:'localhost',
+//     user:'root',
+//     password:'',
+//     database:'empleados'
+// });
 
 pool.query = util.promisify(pool.query)
 module.exports = pool;
